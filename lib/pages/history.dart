11049bf1e66688901943403be33f4aeb32f03e5a
Denny Lianto <c14190004@john.petra.ac.id>
2022-06-29 18:02:00 +0700

    update

---
+++ lib/pages/history.dart
@@ -0,0 +1,93 @@
+// ignore_for_file: prefer_const_constructors, TODO
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../dbservices.dart';
+import '../widgets/youtubeplayer.dart';
+
+class History extends StatefulWidget {
+  const History({Key? key}) : super(key: key);
+
+  @override
+  State<History> createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+  late CollectionReference _colRefContentHistory;
+  var uid = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    final userUID = FirebaseAuth.instance.currentUser!.uid;
+    _colRefContentHistory = FirebaseFirestore.instance
+        .collection('users')
+        .doc(userUID)
+        .collection('history');
+
+    uid = userUID;
+
+    super.initState();
+  }
+
+  Stream<QuerySnapshot<Object?>> Data(
+      CollectionReference<Object?> colRefContentBasic) {
+    if (mounted) {
+      setState(() {});
+    }
+    return Database.getContent(colRefContentBasic);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('HIIIIIIIII $uid');
+    return SafeArea(
+      child: Expanded(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('History'),
+            backgroundColor: Color(0xFF171717),
+          ),
+          body: Container(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: Data(_colRefContentHistory),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error');
+                } else if (snapshot.hasData || snapshot.data != null) {
+                  // print(snapshot.data!.docs.length);
+                  return Expanded(
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        DocumentSnapshot contentDs = snapshot.data!.docs[index];
+
+                        return Center(
+                          child: YtPlayer(Youtube_link: contentDs['link']),
+                        );
+                      },
+                      itemCount: snapshot.data!.docs.length,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return Divider(height: 10);
+                      },
+                    ),
+                  );
+                }
+
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      Colors.white70,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
